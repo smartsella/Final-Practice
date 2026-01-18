b0a25c6aa9d1ee1751dec680781fd3385d60de92
selladurai <selladurai1904@gmail.com>
2026-01-18 21:55:08 +0530

    js problem

--- JS/oops.js
+++ JS/oops.js
@@ -1,8 +1,8 @@
-//class
-//object
-//Encapsulaton
-//Inheritance
-//polybornism
-//abstraction
-//classes
-//object
+//1) class
+//2) object
+//3) Encapsulaton
+//4) Inheritance
+//5) polybornism
+//6) abstraction
+//7) classes
+//8) object
