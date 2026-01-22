a805cff32cb57612689b2385e628c0d4642ce4a3
selladurai <selladurai1904@gmail.com>
2026-01-22 21:23:05 +0530

    js problem

--- JS/looping.js
+++ JS/looping.js
@@ -4,9 +4,9 @@ for (i = 1; i <= 20; i += 2) {
 }
 console.log(i);
 
-// // for (let i = 1; i <= 20; i += 2) {
-// //   console.log(i);
-// // }
+for (let i = 1; i <= 20; i += 2) {
+  console.log(i);
+}
 
 // // let a = 0;
 // // for (let i = 0; i <= 10; i++) {
