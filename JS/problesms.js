f38d0c2da5fd7135440c37c32674592fbf50ec41
selladurai <selladurai1904@gmail.com>
2026-01-26 21:45:04 +0530

    js sums

--- JS/problesms.js
+++ JS/problesms.js
@@ -1,20 +1,20 @@
-// //find minimum and maximum num
-
-// const findMinMax=(arr)=> {
-//   let min = arr[0];
-//   let max = arr[0];
-
-//   for (let i = 1; i < arr.length; i++) {
-//     if (arr[i] < min) min = arr[i];
-//     if (arr[i] > max) max = arr[i];
-//   }
-
-//   return { min, max };
-// }
-
-// console.log(findMinMax([3, 5, 1, 9, 2]));
+//find minimum and maximum num
 
 const findMinMax = (arr) => {
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
+  }
+
+  return { min, max };
+};
+
+console.log(findMinMax([3, 5, 1, 9, 2]));
+
+const findMinMax1 = (arr) => {
   let min = arr[0];
   let max = arr[0];
 
